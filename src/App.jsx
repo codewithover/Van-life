2c3463b9e47b5e35aae4c1433e4645d8ac6807fb
Shivam chauhan <shivam08chauhan@gmail.com>
2025-08-11 15:05:34 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { BrowserRouter , Routes , Route, Link} from 'react-router-dom'
+import Home from './assets/pages/Home'
+import About from './assets/pages/About'
+function App() {
+  
+  return (
+    <BrowserRouter>
+        <header>
+          <Link className='site-logo' to='/'>#VANLIFE</Link>
+          <nav>
+            <Link to='/about'>About</Link>
+          </nav>
+        </header>
+
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+      </Routes>
+    </BrowserRouter>
+  )
+   
+}
+
+export default App
